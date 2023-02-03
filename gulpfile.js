08389b23fbbf90ff1746a5ff9e378ccb6f05092e
msejak <7o.mseJak@gmail.com>
2023-02-03 12:41:49 +0300

    first deploy

--- gulpfile.js
+++ gulpfile.js
@@ -8,32 +8,27 @@ const rename = require('gulp-rename');
 gulp.task('server', function () {
     browserSync({
         server: {
-            // TODO: поменять путь!
             baseDir: 'src',
         },
     });
 
-    // TODO: поменять путь!
     gulp.watch('src/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('styles', function () {
     return (
         gulp
-            // TODO: поменять путь!
             .src('src/sass/**/*.+(scss|sass)')
             .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
             .pipe(rename({ suffix: '.min', prefix: '' }))
             .pipe(autoprefixer())
             .pipe(cleanCSS({ compatibility: 'ie8' }))
-            // TODO: поменять путь!
             .pipe(gulp.dest('src/css'))
             .pipe(browserSync.stream())
     );
 });
 
 gulp.task('watch', function () {
-    // TODO: поменять путь!
     gulp.watch('src/sass/**/*.+(scss|sass)', gulp.parallel('styles'));
 });
 
