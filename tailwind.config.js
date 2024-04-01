c4781b8bc1c8841b7637dc2afe1dece8c7b9f7da
msejak <7o.mseJak@gmail.com>
2024-04-01 19:55:30 +0300

    fix: modifyied breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    screens: {
+      sm: '576px',
+      // => @media (min-width: 576px) { ... }
+
+      md: '992px',
+      // => @media (min-width: 992px) { ... }
+    },
   },
   plugins: [],
 };
