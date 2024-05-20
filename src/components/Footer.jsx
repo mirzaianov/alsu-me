ac9a4a2c8522316172151206af7fd178c424a03f
mirzaianov <7o.mseJak@gmail.com>
2024-05-20 19:45:29 +0300

    fix(KAN-30): Start Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,30 @@
+import Divider from './Divider';
+import Logo from './Logo';
+import Navbar from './NavBar';
+
 const Footer = () => {
   return (
     <section
       className={`mb-[var(--xl)] flex w-full max-w-[var(--container-mobile)] flex-col items-center justify-start gap-[var(--l)] px-[var(--s)]`}
     >
-      Footer
+      <article>
+        <div>
+          <Logo />
+        </div>
+        <div>
+          <div>
+            <p>Алсу Каримова</p>
+            <p>
+              Дипломированный преподаватель и переводчик английского и немецкого
+              языков с многолетним опытом профессиональной деятельности в
+              международных компаниях России и США
+            </p>
+          </div>
+          <Navbar type="block-2" />
+        </div>
+      </article>
+      <Divider />
+      <article></article>
     </section>
   );
 };
