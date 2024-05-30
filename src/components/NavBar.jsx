6f4a6ceaaddabd6715f5d67f81120e288234c793
mirzaianov <7o.mseJak@gmail.com>
2024-05-30 18:23:14 +0300

    feat(KAN-44): Complete Navbar effects

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import propTypes from 'prop-types';
 
 const items = [
@@ -10,6 +11,30 @@ const items = [
 ];
 
 const Navbar = ({ type = '', setIsDropdownOpen, isDropdownOpen }) => {
+  const [activeLink, setActiveLink] = useState('');
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveLink(entry.target.id);
+        }
+      });
+    });
+
+    items.forEach((item) => {
+      const section = document.getElementById(item[0]);
+      if (section) observer.observe(section);
+    });
+
+    return () => {
+      items.forEach((item) => {
+        const section = document.getElementById(item[0]);
+        if (section) observer.unobserve(section);
+      });
+    };
+  }, []);
+
   let ULstyle = {};
   let LIstyle = {};
 
@@ -25,7 +50,6 @@ const Navbar = ({ type = '', setIsDropdownOpen, isDropdownOpen }) => {
       paddingInline: 'var(--l)',
       minWidth: '460px',
       maxWidth: '640px',
-      // flex: '1 1 auto',
       margin: '0 auto',
     };
 
@@ -65,19 +89,18 @@ const Navbar = ({ type = '', setIsDropdownOpen, isDropdownOpen }) => {
     };
   }
 
-  const handleClick = () => {
+  const handleClick = (id) => {
     if (setIsDropdownOpen) {
       setIsDropdownOpen(!isDropdownOpen);
     }
+
+    setActiveLink(id);
   };
 
   return (
     <nav className="w-full">
       <ul
-        className="
-          grid
-          text-body-bold
-        "
+        className="grid text-body-bold"
         style={{ ...ULstyle }}
       >
         {items.map((item) => (
@@ -85,9 +108,22 @@ const Navbar = ({ type = '', setIsDropdownOpen, isDropdownOpen }) => {
             style={{ ...LIstyle, gridArea: item[0] }}
             id={item[0]}
             key={item[0]}
-            onClick={handleClick}
+            onClick={() => handleClick(item[0])}
           >
-            <a href={`#${item[0]}`}>{item[1]}</a>
+            <a
+              className={`relative inline-block hover:text-text-90/50 ${activeLink === item[0] ? 'active' : ''}`}
+              href={`#${item[0]}`}
+            >
+              {item[1]}
+              <span
+                className={`absolute bottom-[-2px] left-0 right-0 h-[4px] rounded-sm bg-current transition-transform duration-300 ease-in-out ${activeLink === item[0] ? 'scale-x-100' : 'scale-x-0'}`}
+                style={{
+                  marginLeft: '-0.25rem',
+                  marginRight: '-0.25rem',
+                  width: 'calc(100% + 0.5rem)',
+                }}
+              />
+            </a>
           </li>
         ))}
       </ul>
