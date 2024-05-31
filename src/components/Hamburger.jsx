b8b7857181e9f8f5062cf1bb938ea4d080919d2d
mirzaianov <7o.mseJak@gmail.com>
2024-05-31 16:46:43 +0300

    feat(KAN-46): Update Hamburger animation (Dropdown is broken)

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,14 +1,35 @@
+import { useEffect, useState } from 'react';
 import propTypes from 'prop-types';
-import hamburger from '../assets/icons/hamburger.svg';
+// import hamburger from '../assets/icons/hamburger.svg';
 
 const Hamburger = ({ onClick }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = async () => {
+    setIsClicked(!isClicked);
+    // onClick();
+  };
+
   return (
-    <button onClick={onClick}>
-      <img
-        className={`h-[var(--xl)]`}
-        src={hamburger}
-        alt="close"
-      />
+    <button onClick={handleClick}>
+      <svg
+        className={`ham hamRotate ham4 ${isClicked ? 'active' : ''}`}
+        viewBox="17.5 17.5 65 65"
+        width="40"
+      >
+        <path
+          className="line top"
+          d="m 70,33 h -40 c 0,0 -8.5,-0.149796 -8.5,8.5 0,8.649796 8.5,8.5 8.5,8.5 h 20 v -20"
+        />
+        <path
+          className="line middle"
+          d="m 70,50 h -40"
+        />
+        <path
+          className="line bottom"
+          d="m 30,67 h 40 c 0,0 8.5,0.149796 8.5,-8.5 0,-8.649796 -8.5,-8.5 -8.5,-8.5 h -20 v 20"
+        />
+      </svg>
     </button>
   );
 };
