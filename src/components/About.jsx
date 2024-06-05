654d3fb0941cff364de6a793d7cede7e1a49ce14
mirzaianov <7o.mseJak@gmail.com>
2024-06-05 17:51:22 +0300

    fix(KAN-7): Corrected InfiniteLogos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
       id="about"
       className="relative my-[var(--xl)] flex w-full max-w-[var(--container-mobile)] scroll-mt-36 flex-col items-center justify-start gap-[var(--l)] px-[var(--s)] tablet:max-w-[var(--container-tablet)] tablet:px-0"
     >
-      <div className="p-fade-in animate-icon-fade-in-right absolute right-8 top-8 tablet:right-16 tablet:top-16">
+      <div className="p-fade-in animate-icon-fade-in-right absolute right-8 top-8 tablet:right-8 tablet:top-20">
         <img
           className="size-20"
           src={superman}
@@ -23,11 +23,10 @@ const About = () => {
       <h2 className="h2-fade-in animate-h2-fade-in text-heading-s tablet:text-heading-m">
         Обо мне
       </h2>
-      <article className="pop-up relative -mt-4 mb-[82px] flex animate-pop-up items-center justify-start">
+      <article className="pop-up relative -mt-4 mb-[82px] flex animate-pop-up items-center justify-start tablet:mt-0">
         <div className="absolute -right-10 -top-2 h-40 w-40 animate-blob rounded-full bg-primary-30 opacity-70 mix-blend-multiply blur-xl filter [animation-delay:_0.5s]"></div>
         <div className="absolute -left-5 -top-1 h-40 w-40 animate-blob rounded-full bg-primary-20 opacity-70 mix-blend-multiply blur-xl filter [animation-delay:_2s]"></div>
         <div className="absolute -bottom-5 h-40 w-40 animate-blob rounded-full bg-primary-40 mix-blend-multiply blur-xl filter [animation-delay:_4s]"></div>
-
         <PhotoCardAbout />
         <div className="absolute -right-[70px] top-[150px] animate-waving-badge-3">
           <Badge
