20ecbcb903780ba02332bedf5dc36b3868d6e75c
mirzaianov <7o.mseJak@gmail.com>
2024-06-07 18:04:39 +0300

    feat(KAN-19): Complete Testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -4,12 +4,22 @@ const Testimonials = () => {
   return (
     <section
       id="testimonials"
-      className="my-[var(--xl)] flex w-full max-w-[var(--container-mobile)] scroll-mt-36 flex-col items-center justify-start gap-[var(--l)] px-[var(--s)] tablet:max-w-[var(--container-tablet)] tablet:gap-[var(--xl)] tablet:px-0"
+      className="my-[var(--xl)] flex w-full max-w-[var(--container-mobile)] scroll-mt-36 flex-col items-center justify-start gap-[var(--l)] px-[var(--s)]
+                tablet:max-w-[var(--container-tablet)] tablet:gap-[var(--xl)] tablet:px-[var(--xl)]
+                desktop:max-w-[var(--container-desktop)] desktop:px-[var(--2xl)]"
     >
-      <h2 className="h2-fade-in animate-h2-fade-in text-center text-heading-s tablet:text-heading-m">
+      <h2
+        className="text-center text-heading-s
+                    tablet:text-heading-m
+                    desktop:text-heading-l"
+      >
         Что говорят мои ученики и клиенты
       </h2>
-      <article className="flex flex-col justify-start gap-[var(--m)]">
+      <article
+        className="flex max-w-[var(--mobile-text-wrapper)] flex-col justify-start gap-[var(--m)]
+                      tablet:max-w-[var(--tablet-text-wrapper)]
+                      desktop:max-w-[var(--desktop-text-wrapper)]"
+      >
         <p className="p-fade-in animate-p-fade-in">
           Мои ученики достигли значительных результатов благодаря нашей
           совместной работе, и я горжусь их успехами.
@@ -19,7 +29,7 @@ const Testimonials = () => {
           устным синхронным переводам.
         </p>
       </article>
-      <article className="pop-up w-screen animate-pop-up">
+      <article className="w-screen">
         <InfiniteTestimonialCards />
       </article>
     </section>
