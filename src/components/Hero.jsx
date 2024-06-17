0957c6f0d6c19e9f85219fa1ad3e3d5907b5ca2f
mirzaianov <7o.mseJak@gmail.com>
2024-06-17 19:24:07 +0300

    fix: Complete Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,16 +19,19 @@ const Hero = ({ width }) => {
     >
       <div className="absolute inset-0 -z-10">
         <div
-          className="absolute -right-[16px] -top-[80px] size-[200px] animate-blob rounded-full bg-primary-20 mix-blend-multiply blur-[75px] filter [animation-delay:_0.5s]
-                          tablet:-right-16 tablet:-top-8 tablet:size-80 tablet:animate-blob-tablet  tablet:[animation-delay:_0.5s]"
+          className="absolute -right-[16px] -top-[80px] size-[200px] animate-blob rounded-full bg-primary-20 mix-blend-multiply blur-[75px] filter tablet:-right-[140px]
+                           tablet:-top-[104px] tablet:size-[400px] tablet:animate-blob-tablet tablet:blur-[150px]
+                           desktop:-right-[60px] desktop:-top-[104px] desktop:size-[400px] desktop:animate-blob-tablet desktop:blur-[150px]"
         ></div>
         <div
-          className="animate-blob2 absolute left-1/2 top-1/2 size-[200px] rounded-full bg-primary-10 opacity-70 mix-blend-multiply blur-[75px] filter [animation-delay:_2s]
-                          tablet:-left-10 tablet:-top-4 tablet:size-80 tablet:animate-blob-tablet tablet:opacity-50 tablet:[animation-delay:_2s]"
+          className="animate-blob2 tablet:animate-blob2-tablet desktop:animate-blob2-tablet absolute left-1/2 top-[calc(50%-40px)] size-[200px] -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-primary-10 opacity-70 mix-blend-multiply blur-[75px] filter tablet:top-[calc(50%-52px)]
+                           tablet:size-[400px] tablet:blur-[150px]
+                           desktop:top-[calc(50%-52px)] desktop:size-[400px] desktop:blur-[150px]"
         ></div>
         <div
-          className="animate-blob3 absolute -left-[16px] bottom-0 size-[200px] rounded-full bg-primary-30 opacity-70 mix-blend-multiply blur-[75px] filter [animation-delay:_5s]
-                          tablet:-bottom-10 tablet:left-8 tablet:size-80 tablet:animate-blob-tablet tablet:opacity-50 tablet:[animation-delay:_5s]"
+          className="animate-blob3 tablet:animate-blob3-tablet desktop:animate-blob3-tablet absolute -left-[16px] bottom-0 size-[200px] rounded-full bg-primary-30 opacity-70 mix-blend-multiply blur-[75px] filter tablet:-left-[140px]
+                           tablet:size-[400px] tablet:blur-[150px]
+                           desktop:-left-[60px] desktop:size-[400px] desktop:blur-[150px]"
         ></div>
       </div>
       <article
