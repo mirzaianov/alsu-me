47228e84d8b1b9ebdb21f70eb68f73b6a54717d0
mirzaianov <7o.mseJak@gmail.com>
2024-06-24 15:06:15 +0300

    fix: Correct centering

--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -44,16 +44,18 @@ const TestimonialCard = ({
 
   return (
     <div className="relative flex h-[400px] w-[var(--card-width)] min-w-[var(--card-width)] flex-col justify-start gap-[var(--m)] rounded-[var(--s)] bg-neutral-0 p-[var(--m)] text-body-tight shadow-[5px_5px_25px_0px_rgba(0,0,0,0.25)]">
-      <div className="flex gap-[var(--s)]">
-        <img
-          className="aspect-square h-[56px] rounded-full object-cover"
-          src={src}
-          alt={fullName}
-          loading="lazy"
-        />
-        <div className="flex min-h-[64px] flex-col justify-center gap-[var(--2xs)]">
-          <p className="text-body-bold">{fullName}</p>
-          <p className="text-wrap">{occupation}</p>
+      <div className="min-h-[64px]">
+        <div className="flex gap-[var(--s)]">
+          <img
+            className="aspect-square h-[56px] rounded-full object-cover"
+            src={src}
+            alt={fullName}
+            loading="lazy"
+          />
+          <div className="flex flex-col justify-center gap-[var(--2xs)]">
+            <p className="text-body-bold">{fullName}</p>
+            <p className="text-wrap">{occupation}</p>
+          </div>
         </div>
       </div>
       <p
