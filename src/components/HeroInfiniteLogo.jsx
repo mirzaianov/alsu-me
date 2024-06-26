8c014612bdd85b79965cfd8c6f268395f7b94673
mirzaianov <7o.mseJak@gmail.com>
2024-06-26 16:17:47 +0300

    fix: Correct InfiniteLogos heigth on mobile

--- src/components/HeroInfiniteLogo.jsx
+++ src/components/HeroInfiniteLogo.jsx
@@ -3,7 +3,9 @@ import propTypes from 'prop-types';
 const HeroInfiniteLogo = ({ src, alt }) => {
   return (
     <img
-      className="my-0 inline-block h-[var(--m)] px-[20px] tablet:h-[var(--xl)] tablet:px-[var(--xl)] desktop:h-[var(--2xl)] desktop:px-[var(--2xl)]"
+      className="my-0 inline-block h-[var(--l)] px-[20px] tablet:h-[var(--xl)]
+                 tablet:px-[var(--xl)]
+                 desktop:h-[var(--2xl)] desktop:px-[var(--2xl)]"
       src={src}
       alt={alt}
     />
