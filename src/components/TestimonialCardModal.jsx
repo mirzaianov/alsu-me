7ea7b6bd2dda942c9181ab326f5056e4a69a61de
mirzaianov <7o.mseJak@gmail.com>
2024-06-28 15:56:34 +0300

    fix(KAN-79): Correct TestimonialCardModal width

--- src/components/TestimonialCardModal.jsx
+++ src/components/TestimonialCardModal.jsx
@@ -27,7 +27,7 @@ const TestimonialCardModal = ({ onClose, children }) => {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-neutral-90/50">
       <div
         ref={closeRef}
-        className="flex max-h-[75vh] max-w-[var(--card-width)] flex-col justify-start gap-[var(--m)] overflow-y-auto rounded-[var(--s)] bg-neutral-0 p-[var(--m)] text-body-tight shadow-[5px_5px_25px_0px_rgba(0,0,0,0.25)]
+        className="flex max-h-[calc(100svh-2*var(--s))] max-w-[calc(100vw-2*var(--s))] flex-col justify-start gap-[var(--m)] overflow-y-auto rounded-[var(--s)] bg-neutral-0 p-[var(--m)] text-body-tight shadow-[5px_5px_25px_0px_rgba(0,0,0,0.25)]
                   tablet:max-w-[51ch]"
       >
         {children}
