c10d1dd94575ee1cd79aa7b25025f027f892d87e
Riaz Mirzaianov <87698216+mirzaianov@users.noreply.github.com>
2025-11-06 11:57:16 +0300

    feat(KAN-115): Add slide-stat animation

--- src/components/HeroPhotoCardBlock.jsx
+++ src/components/HeroPhotoCardBlock.jsx
@@ -6,20 +6,9 @@ import hand from '../assets/icons/hand.svg';
 
 const HeroPhotoCardBlock = ({ width }) => {
   return (
-    <div
-      className="flex animate-hero-foto-fade-in items-center justify-between
-                    desktop:flex-col-reverse desktop:gap-[var(--m)]"
-    >
-      <div
-        className="flex max-w-[160px] items-center justify-center
-                      tablet:min-w-[160px]
-                      desktop:max-w-full"
-      >
-        <ul
-          className="flex w-[100px] flex-col justify-start gap-[var(--s)]
-                        tablet:gap-[var(--l)]
-                        desktop:w-fit desktop:flex-row desktop:gap-[var(--m)]"
-        >
+    <div className="flex animate-hero-foto-fade-in items-center justify-between desktop:flex-col-reverse desktop:gap-[var(--m)]">
+      <div className="flex max-w-[160px] items-center justify-center tablet:min-w-[160px] desktop:max-w-full">
+        <ul className="flex w-[100px] flex-col justify-start gap-[var(--s)] tablet:gap-[var(--l)] desktop:w-fit desktop:flex-row desktop:gap-[var(--m)]">
           <Stat
             quantity="16+"
             text="лет опыта"
@@ -28,23 +17,31 @@ const HeroPhotoCardBlock = ({ width }) => {
             quantity="4+"
             text="года работы в США"
           />
-          <Stat
-            quantity="200+"
-            text="довольных учеников"
-          />
+          <div className="h-[98px] overflow-hidden">
+            <div className="animate-slide-stat flex w-full flex-col items-start">
+              <Stat
+                className="mb-[49px]"
+                quantity="200+"
+                text="довольных учеников"
+              />
+              <Stat
+                className="mb-[49px]"
+                quantity="50+"
+                text="успешных кейсов"
+              />
+              <Stat
+                className="mb-[49px]"
+                quantity="200+"
+                text="довольных учеников"
+              />
+            </div>
+          </div>
         </ul>
       </div>
-      <div
-        className="flex min-w-[178px] items-center justify-center
-                        tablet:min-w-[400px]
-                        desktop:min-w-fit"
-      >
+      <div className="flex min-w-[178px] items-center justify-center tablet:min-w-[400px] desktop:min-w-fit">
         <div className="relative flex items-center justify-start">
           <HeroPhotoCard />
-          <div
-            className="absolute -left-[32px] bottom-[24px] animate-waving-badge-3
-                          tablet:bottom-[40px]"
-          >
+          <div className="absolute -left-[32px] bottom-[24px] animate-waving-badge-3 tablet:bottom-[40px]">
             {width < 577 ? (
               <Badge
                 alt="The hand icon with wavy animation"
